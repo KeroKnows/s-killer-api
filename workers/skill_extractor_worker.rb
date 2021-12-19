10d612723e61a42b8c361732d905a16d77567c4a
joanna <joannawu@nlplab.cc>
2021-12-19 18:33:38 +0800

    fix: correctly process worker jobs

--- workers/skill_extractor_worker.rb
+++ workers/skill_extractor_worker.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-require_relative '../init'
 require 'shoryuken'
+require 'yaml'
+
+require_relative '../init'
 
 # Worker to analyze skills in parallel
 class SkillExtractorWorker
@@ -17,16 +19,40 @@ class SkillExtractorWorker
   include Shoryuken::Worker
   shoryuken_options queue: config.CLONE_QUEUE_URL, auto_delete: true
 
+  PYTHON = 'python3' # may need to change this to `python`, depending on your system
+  SCRIPT = File.join(File.dirname(__FILE__), 'extract.py')
+
   # extract skill and store in database
   def perform(_sqs_msg, jobs)
-    skill_list = jobs.map do |job|
-      if Repository::JobsSkills.job_exist?(job)
-        Repository::JobsSkills.find_skills_by_job_id(job.db_id)
-      else
-        skills = Skiller::Skill::SkillMapper.new(job).skills
-        Repository::JobsSkills.find_or_create(skills)
-      end
+    random_seed = rand(10_000)
+    jobs = JSON.parse jobs
+    jobs.each do |job|
+      job = Skiller::Representer::Job.new(OpenStruct.new).from_json(job.to_json)
+      tmp_file = File.join(File.dirname(__FILE__), ".extractor.#{random_seed}.tmp")
+      File.write(tmp_file, job.description, mode: 'w')
+      script_result = `#{PYTHON} #{SCRIPT} "#{tmp_file}"`
+      File.delete(tmp_file)
+      write_to_db(job, script_result)
     end
-    skill_list.reduce(:+)
+  end
+
+  # store the results to database
+  #  [ TODO ] improve the structure (sorry for the mess here)
+  def write_to_db(job, result)
+    skills = YAML.safe_load(result)
+    salary = Skiller::Value::Salary.new({
+      year_min: job.salary.year_min,
+      year_max: job.salary.year_max,
+      currency: job.salary.currency
+    })
+    skills = skills.map do |skill|
+      Skiller::Entity::Skill.new(
+        id: nil,
+        name: skill,
+        job_db_id: job.db_id,
+        salary: salary
+      )
+    end
+    Skiller::Repository::JobsSkills.find_or_create(skills)
   end
 end
